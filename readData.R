fd4d715ca3d7e63c672328dee5c3d5ec30a770e7
giamac <gmacauda@gmx.net>
2018-02-09 19:07:58 +0100

    some analysis

--- readData.R
+++ readData.R
@@ -4,6 +4,8 @@
 
 source('readFiles.R')
 
+library(lme4)
+
 # Make a list of all the files
 
 list_of_data <- list.files(path = 'data/', full.names = TRUE)
@@ -14,7 +16,27 @@ all_files <- lapply(list_of_data,readFiles)
 
 # Merge them into a big dataFrame
 
-final_df <- do.call('rbind', all_files)
+final_df <- do.call('rbind', all_files) %>%
+  mutate(response_coded = case_when(response == 'left' ~ 1,
+                                    response == 'right' ~ 0))
 
-# Do stuff with the data
-write_csv(final_df,'first_files.csv')
+
+
+m1 <- glmer(congruence ~ 1 + (1 | id), data = final_df, family = binomial)
+mTendency <- glmer(response_coded ~ 1 + (1|id), data = final_df, family = binomial)
+
+
+
+m2 <- glmer(response_coded ~ 1 + peak + (1 + peak| id), data = final_df, family = binomial)
+m3 <- brm(formula = congruence ~ 1, 
+          data = final_df,
+          family = bernoulli())
+
+m3brm <- brm(formula = response_coded ~ 1 + peak, 
+          data = final_df,
+          family = bernoulli())
+m3Tend <- brm(formula = response_coded ~ 1, 
+              data = final_df,
+              family = bernoulli())
+
+factor(final_df$response)
